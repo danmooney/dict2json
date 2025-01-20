f58b8ea6c6cdf8e42937d90b0c91a71d8254d9ac
Dan Mooney <dan.r.mooney@gmail.com>
2025-01-20 09:04:33 -0600

    Highlight lines

--- src/components/InputSection.js
+++ src/components/InputSection.js
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Toolbar from './Toolbar';
 
 function InputSection({ input, setInput, handleFileUpload }) {
+  const [currentLine, setCurrentLine] = useState(0);
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleCursorMove = (e) => {
+    const text = e.target.value;
+    const cursorPosition = e.target.selectionStart;
+    const linesBeforeCursor = text.slice(0, cursorPosition).split('\n');
+    setCurrentLine(linesBeforeCursor.length - 1);
+  };
+
   return (
     <div className="input">
       <div className="section-header">
         <h2>Python Dictionary</h2>
         <Toolbar onFileUpload={handleFileUpload} />
       </div>
-      <div className="editor-container">
+      <div 
+        className={`editor-container ${isFocused ? 'focused' : ''}`}
+        style={{
+          '--current-line': currentLine,
+          '--line-offset': `${10 + currentLine * 24}px`
+        }}
+      >
         <div className="line-numbers">
           {input.split('\n').map((_, i) => (
-            <div key={i} className="line-number">{i + 1}</div>
+            <div key={i} className="line-number">
+              {i + 1}
+            </div>
           ))}
         </div>
         <textarea
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          onKeyUp={handleCursorMove}
+          onClick={handleCursorMove}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
           placeholder="Enter Python dictionary"
           wrap="soft"
           spellCheck="false"
